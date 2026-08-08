eae5fa46134054e4e07e1d9080518ec750296c7c
hclhcl0 <hclhcl0@gmail.com>
2026-08-08 09:24:13 +0700

    fix(db): correct DO block syntax in migration script for site_settings_menu_menu_items id column type

--- scripts/migrations.mjs
+++ scripts/migrations.mjs
@@ -3714,7 +3714,7 @@ export const MIGRATION_STATEMENTS = [
   `DO $$ BEGIN UPDATE "users" SET "lock_until" = NULL, "login_attempts" = 0 WHERE "lock_until" IS NOT NULL; EXCEPTION WHEN others THEN null; END $$`
   ,
   // FIX: site_settings_menu_menu_items ID type change
-  `DO $
+  `DO $$
   BEGIN
     ALTER TABLE "site_settings_menu_menu_items_sub_items" DROP CONSTRAINT IF EXISTS "site_settings_menu_menu_items_sub_items_parent_fk";
 
@@ -3728,7 +3728,5 @@ export const MIGRATION_STATEMENTS = [
     ALTER TABLE "site_settings_menu_menu_items_sub_items"
       ADD CONSTRAINT "site_settings_menu_menu_items_sub_items_parent_fk"
       FOREIGN KEY ("_parent_id") REFERENCES "site_settings_menu_menu_items" ("id") ON DELETE cascade ON UPDATE no action;
-  EXCEPTION WHEN others THEN
-    null;
-  END $;`
+  END $$;`
 ];
